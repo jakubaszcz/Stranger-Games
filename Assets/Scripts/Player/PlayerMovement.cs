9f0f697aab6139da2a6852099518638432ed861b
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-02-15 19:04:42 +0100

    feat: update player with camera space movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,50 +6,93 @@ public class PlayerMovement : MonoBehaviour
     // Player chars
     [SerializeField] private float p_Speed = 10f;
     [SerializeField] private float p_JumpForce = 10f;
-
+    [SerializeField] private float p_Sensitivity = 3f;
+    [SerializeField] private float p_XRotation = 0f;
+    
+    // References
+    [SerializeField] private Transform p_cameraTransform;
+    
     // Player input
     private PlayerInputActions p_Actions;
     private Rigidbody p_Rigidbody;
     
     // Player movements
     private Vector2 p_MoveInput;
+    private Vector3 p_LookInput;
     private bool p_JumpInput;
 
+    private void Start()
+    {
+        // Lock the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
     private void Awake()
     {
+        // Initialize player
         p_Actions = new PlayerInputActions();
         p_Rigidbody = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        Vector3 dir = new Vector3(p_MoveInput.x, 0f, p_MoveInput.y);
+        // Movement
+        MovementUpdate();
+        // Jump
+        if (p_JumpInput) JumpUpdate();
+    }
+
+    private void MovementUpdate()
+    {
+        // Previous code was about the world space coordinates
+        // Now we use the camera space coordinates
+        Vector3 forward = p_cameraTransform.forward;
+        Vector3 right = p_cameraTransform.right;
+        
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+        
+        Vector3 dir = forward * p_MoveInput.y + right * p_MoveInput.x;
         
         Vector3 velocity = dir * p_Speed;
         velocity.y = p_Rigidbody.linearVelocity.y;
         
         p_Rigidbody.linearVelocity = velocity;
+    }
 
-        if (p_JumpInput)
-        {
-            p_Rigidbody.AddForce(Vector3.up * p_JumpForce, ForceMode.Impulse);
-            p_JumpInput = false;
-        }
+    private void JumpUpdate()
+    {
+        p_Rigidbody.AddForce(Vector3.up * p_JumpForce, ForceMode.Impulse);
+        p_JumpInput = false;
     }
 
     private void OnEnable()
     {
         p_Actions.Enable();
+        // Movement
         p_Actions.Player.Move.performed += OnMove;
         p_Actions.Player.Move.canceled += OnMove;
+        
+        // Jump
         p_Actions.Player.Jump.performed += OnJump;
+        
+        // Look
+        p_Actions.Player.Look.performed += OnLook;
     }
 
     private void OnDisable()
     {
+        // Movement
         p_Actions.Player.Move.performed -= OnMove;
         p_Actions.Player.Move.canceled -= OnMove;
+        
+        // Jump
         p_Actions.Player.Jump.performed -= OnJump;
+        
+        // Look
+        p_Actions.Player.Look.performed -= OnLook;
         p_Actions.Disable();
     }
 
@@ -62,5 +105,23 @@ public class PlayerMovement : MonoBehaviour
     private void OnJump(InputAction.CallbackContext ctx)
     {
         p_JumpInput = ctx.ReadValueAsButton();
+    }
+
+    private void OnLook(InputAction.CallbackContext ctx)
+    {
+        p_LookInput = ctx.ReadValue<Vector2>();
+        
+        float mouseX = p_LookInput.x * p_Sensitivity;
+        float mouseY = p_LookInput.y * p_Sensitivity;
+
+        // Vertical
+        p_XRotation -= mouseY;
+        p_XRotation = Mathf.Clamp(p_XRotation, -80f, 80f);
+
+        p_cameraTransform.localRotation =
+            Quaternion.Euler(p_XRotation, 0f, 0f);
+
+        // Horizontal
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
