7622ca06ac0a59626e357af3004fd34ad3074132
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-02-17 21:48:05 +0100

    add: make the coin a prefab

--- Assets/Scripts/Other/CoinCollision.cs
+++ Assets/Scripts/Other/CoinCollision.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class CoinCollision : MonoBehaviour
 {
+    // Detect collision with a player
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            // Invoke OnCoin signal
             GameSignals.OnCoin?.Invoke();
+            
+            // Then destroys the coin
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    // Player statistics
     private int _coins = 0;
     
-    public int GetCoins() { return _coins; }
+    // public int GetCoins() { return _coins; }
     
+    // Add coins function signal
     void AddCoin() { _coins++; }
     
+    // Enable signals
     void OnEnable()
     {
         GameSignals.OnCoin += AddCoin;
     }
     
+    // Disable signals
     void OnDisable()
     {
         GameSignals.OnCoin += AddCoin;
